214c297ee0d317d607b4e521ffa64031ed86d789
Hrishikesh R Prasad <hrishikeshr.cd23@bmsce.ac.in>
2024-12-16 09:51:49 +0530

    Update program6a.c

--- program6a.c
+++ program6a.c
@@ -6,99 +6,94 @@ struct node {
     struct node* next;
 };
 
-struct node* createNode(int data) {
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    if (!newNode) {
-        printf("Memory error\n");
-        return NULL;
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+struct node* head = NULL;
+
+void insert(int data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = head;
+    head = new_node;
 }
 
-struct node* sortList(struct node* head) {
-    struct node* current = head;
+void printList() {
+    struct node* temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void reverse() {
+    struct node *prev, *curr, *nextn;
+    prev = NULL;
+    curr = nextn = head;
+    while(nextn != NULL) {
+        nextn = nextn->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextn;
+    }
+    head = prev;
+}
+
+void sort() {
+    struct node *i, *j;
     int temp;
-    while (current != NULL) {
-        struct node* index = current;
-        while (index->next != NULL) {
-            if (current->data > index->next->data) {
-                temp = current->data;
-                current->data = index->next->data;
-                index->next->data = temp;
+    for(i = head; i->next != NULL; i = i->next) {
+        for(j = i->next; j != NULL; j = j->next) {
+            if(i->data > j->data) {
+                temp = i->data;
+                i->data = j->data;
+                j->data = temp;
             }
-            index = index->next;
         }
-        current = current->next;
     }
-    return head;
 }
 
-struct node* reverseList(struct node* head) {
-    struct node* prev = NULL;
-    struct node* current = head;
-    struct node* next = NULL;
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+void concat(struct node *head1, struct node *head2) {
+    struct node *temp1, *temp2;
+    temp1 = head1;
+    temp2 = head2;
+    while(temp1->next != NULL) {
+        temp1 = temp1->next;
     }
-    return prev;
-}
-
-struct node* concatenateLists(struct node* head1, struct node* head2) {
-    if (head1 == NULL)
-        return head2;
-    if (head2 == NULL)
-        return head1;
-    struct node* current = head1;
-    while (current->next != NULL)
-        current = current->next;
-    current->next = head2;
-    return head1;
-}
-
-void printList(struct node* head) {
-    struct node* current = head;
-    while (current != NULL) {
-        printf("%d -> ", current->data);
-        current = current->next;
+    temp1->next = temp2;
+    printf("\n the concatenated list is \n");
+    temp1 = head1;
+    while(temp1 != NULL) {
+        printf("%d", temp1->data);
+        temp1 = temp1->next;
     }
-    printf("NULL\n");
 }
 
 void main() {
-    int n1, n2, n3, n4, n5, n6;
-    printf("Enter the data for List 1 (3 nodes): ");
-    scanf("%d %d %d", &n1, &n2, &n3);
-
-    printf("Enter the data for List 2 (3 nodes): ");
-    scanf("%d %d %d", &n4, &n5, &n6);
-
-    struct node* head1 = createNode(n1);
-    head1->next = createNode(n2);
-    head1->next->next = createNode(n3);
-
-    struct node* head2 = createNode(n4);
-    head2->next = createNode(n5);
-    head2->next->next = createNode(n6);
-
-    printf("List 1 before sorting: ");
-    printList(head1);
-    head1 = sortList(head1);
-    printf("List 1 after sorting: ");
-    printList(head1);
-
+    int data;
+    printf("Enter the number of elements: ");
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &data);
+        insert(data);
+    }
+    printf("Original List: ");
+    printList();
+    reverse();
+    printf("Reversed List: ");
+    printList();
+    sort();
+    printf("Sorted List: ");
+    printList();
+    struct node* head2 = NULL;
+    printf("Enter the number of elements for list 2: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        printf("Enter element %d for list 2: ", i+1);
+        scanf("%d", &data);
+        insert(data);
+    }
     printf("List 2: ");
-    printList(head2);
-
-    struct node* concatenatedList = concatenateLists(head1, head2);
-    printf("Concatenated List: ");
-    printList(concatenatedList);
-
-    struct node* reversedList = reverseList(concatenatedList);
-    printf("Reversed Concatenated List: ");
-    printList(reversedList);
+    printList();
+    concat(head, head2);
 }
