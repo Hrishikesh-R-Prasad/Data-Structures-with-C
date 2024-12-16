02d19220b685d01bd6aa0e5600c0e50f2675327a
Hrishikesh R Prasad <hrishikeshr.cd23@bmsce.ac.in>
2024-12-16 12:41:28 +0530

    Update program7.c

--- program7.c
+++ program7.c
@@ -8,15 +8,21 @@ struct node {
 };
 
 struct node *head = NULL;
-struct node *tail = NULL;
 
-void insertBeg(int x) {
+void insertBeg() {
+    int x;
+    printf("Enter the value to be inserted : ");
+    scanf("%d", &x);
     struct node *newnode;
     newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
     newnode->data = x;
     newnode->prev = NULL;
-    if(head == NULL) {
-        head = tail = newnode;
+    if (head == NULL) {
+        head = newnode;
         newnode->next = NULL;
     } else {
         newnode->next = head;
@@ -25,58 +31,79 @@ void insertBeg(int x) {
     }
 }
 
-void insertPos(int x) {
+void insertPos() {
+    int x;
+    printf("Enter the value to be inserted : ");
+    scanf("%d", &x);
     struct node *newnode, *temp;
     int pos;
     printf("Enter the position: ");
     scanf("%d", &pos);
-    if(pos == 1) {
-        insertBeg(x);
+
+    if (pos == 1) {
+        insertBeg();
+        return;
     } else {
         newnode = (struct node *)malloc(sizeof(struct node));
+        if (newnode == NULL) {
+            printf("Memory allocation failed.\n");
+            return;
+        }
         newnode->data = x;
         newnode->next = NULL;
         newnode->prev = NULL;
+
         temp = head;
-        for(int i = 1; i < pos-1 ; i++) {
+        for (int i = 1; i < pos - 1; i++) {
             temp = temp->next;
-            if(temp == NULL) {
-                printf("There are less than %d nodes", pos);
+            if (temp == NULL) {
+                printf("There are less than %d nodes\n", pos);
+                free(newnode); // Avoid memory leak
                 return;
             }
         }
         newnode->prev = temp;
         newnode->next = temp->next;
+        if (temp->next != NULL) {
+            temp->next->prev = newnode;
+        }
         temp->next = newnode;
-        if(newnode->next != NULL)
-            newnode->next->prev = newnode;
-        else
-            tail = newnode;
     }
 }
 
-void insertEnd(int x) {
-    struct node *newnode;
+void insertEnd() {
+    int x;
+    printf("Enter the value to be inserted : ");
+    scanf("%d", &x);
+    struct node *newnode, *temp;
     newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
     newnode->data = x;
     newnode->next = NULL;
-    if(head == NULL) {
-        head = tail = newnode;
+
+    if (head == NULL) {
+        head = newnode;
         newnode->prev = NULL;
     } else {
-        newnode->prev = tail;
-        tail->next = newnode;
-        tail = newnode;
+        temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->prev = temp;
     }
 }
 
 void display() {
     struct node *temp;
-    if(head == NULL) {
+    if (head == NULL) {
         printf("List is empty\n");
     } else {
         temp = head;
-        while(temp != NULL) {
+        while (temp != NULL) {
             printf("%d ", temp->data);
             temp = temp->next;
         }
@@ -85,8 +112,8 @@ void display() {
 }
 
 void main() {
-    int choice, x, pos;
-    while(1) {
+    int choice;
+    while (1) {
         printf("1. Insert at beginning\n");
         printf("2. Insert at position\n");
         printf("3. Insert at end\n");
@@ -94,21 +121,15 @@ void main() {
         printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch(choice) {
+        switch (choice) {
             case 1:
-                printf("Enter the value: ");
-                scanf("%d", &x);
-                insertBeg(x);
+                insertBeg();
                 break;
             case 2:
-                printf("Enter the value: ");
-                scanf("%d", &x);
-                insertPos(x);
+                insertPos();
                 break;
             case 3:
-                printf("Enter the value: ");
-                scanf("%d", &x);
-                insertEnd(x);
+                insertEnd();
                 break;
             case 4:
                 display();
